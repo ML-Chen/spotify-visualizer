f20a7ae8ab4178736c6a7b768ca96eff6fc5aea2
Michael Chen <michael.ld.chen@gmail.com>
2020-01-21 01:08:02 -0500

    Use MONGODB_URI instead of MONGODB_URI_LOCAL

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -13,7 +13,9 @@ export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
-export const MONGODB_URI = prod ? process.env["MONGODB_URI"] : process.env["MONGODB_URI_LOCAL"];
+// export const MONGODB_URI = prod ? process.env["MONGODB_URI"] : process.env["MONGODB_URI_LOCAL"];
+export const MONGODB_URI = process.env["MONGODB_URI"];
+
 
 if (!SESSION_SECRET) {
     logger.error("No client secret. Set SESSION_SECRET environment variable.");
